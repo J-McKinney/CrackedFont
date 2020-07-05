04c6855b1ac332a294154022b0a142d37b619220
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-05 19:18:14 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -7,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper">
+          <div className="cracked">
+            <span>cracked</span>
+            <span>cracked</span>
+            <span>cracked</span>
+          </div>
+        </div>
       </header>
     </div>
   );
