c977b6000045fda9a5662babc8f415fa9258cf64
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-05 15:50:25 -0400

    Updates

---
+++ precache-manifest.6f3aaad72eea29045b69407fa0999ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e901b0226addcc3b141cc29bde9d1e8",
+    "url": "/CrackedFont/index.html"
+  },
+  {
+    "revision": "6c4a27847d505ad92e02",
+    "url": "/CrackedFont/static/css/main.dc403b88.chunk.css"
+  },
+  {
+    "revision": "b934e9fc04765a4cabca",
+    "url": "/CrackedFont/static/js/2.83571397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CrackedFont/static/js/2.83571397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4a27847d505ad92e02",
+    "url": "/CrackedFont/static/js/main.67753f54.chunk.js"
+  },
+  {
+    "revision": "5f736fc63dc6e7a882a8",
+    "url": "/CrackedFont/static/js/runtime-main.056437aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CrackedFont/static/media/logo.5d5d9eef.svg"
+  }
+]);
