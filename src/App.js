f1b3fd1c714905df22ccbcd112dbf4ba011317dc
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-05 15:57:20 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import ReactLogo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={ReactLogo} className="App-logo" alt="React-Logo" />
           <div className="wrapper">
             <div className="cracked">
               <span>Cracked Font</span>
